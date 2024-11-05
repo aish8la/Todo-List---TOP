dfa5b93f239203d95193e16d4825774f23729e63
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-06 00:14:06 +0500

    add a task constructor in a seperate ESM module

---
+++ src/taskManager.js
@@ -0,0 +1,12 @@
+class taskConstructorClass {
+    constructor(taskID, taskName, description, priority, project, dueDate, subtaskObj, completed) {
+        this.taskID = taskID;
+        this.taskName = taskName;
+        this.description = description;
+        this.priority = priority;
+        this.project = project;
+        this.dueDate = dueDate;
+        this.subtaskObj = subtaskObj;
+        this.completed = completed;
+    }
+}
