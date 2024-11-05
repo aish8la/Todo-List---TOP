0d734384a4edd1c1ac6e79aee7471b91e9129e70
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-06 01:07:13 +0500

    change to private fields and add setters

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,12 +1,53 @@
-class taskConstructorClass {
+export class taskConstructorClass {
     constructor(taskID, taskName, description, priority, project, dueDate, subtaskObj, completed) {
-        this.taskID = taskID;
-        this.taskName = taskName;
-        this.description = description;
-        this.priority = priority;
-        this.project = project;
-        this.dueDate = dueDate;
-        this.subtaskObj = subtaskObj;
-        this.completed = completed;
+        this.#taskID = taskID;
+        this.#taskName = taskName;
+        this.#description = description;
+        this.#priority = priority;
+        this.#project = project;
+        this.#dueDate = dueDate;
+        this.#subtaskObj = subtaskObj;
+        this.#completed = completed;
+    }
+
+    #taskID;
+    #taskName;
+    #description;
+    #priority;
+    #project;
+    #dueDate;
+    #subtaskObj;
+    #completed;
+
+    
+set taskName(name) {
+    this.#taskName = name;
+}
+
+set description(description) {
+    this.#description = description;
+}
+
+set priority(priority) {
+    this.#priority = priority;
+}
+
+set project(project) {
+    this.#project = project;
+}
+
+set dueDate(dueDate) {
+    this.#dueDate = dueDate;
+}
+
+// Implement subtask feature here !!!
+
+set completed(completeStatus) {
+        if(typeof(completeStatus) === 'boolean') {
+            this.#completed = completeStatus;
+        };
     }
 }
+
+// dummy dependencies
+
