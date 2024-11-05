49b530bc5f2bda284c63d04864d732ef932c61c4
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-06 01:16:35 +0500

    add getters

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,53 +1,88 @@
 export class taskConstructorClass {
-    constructor(taskID, taskName, description, priority, project, dueDate, subtaskObj, completed) {
-        this.#taskID = taskID;
-        this.#taskName = taskName;
-        this.#description = description;
-        this.#priority = priority;
-        this.#project = project;
-        this.#dueDate = dueDate;
-        this.#subtaskObj = subtaskObj;
-        this.#completed = completed;
-    }
-
-    #taskID;
-    #taskName;
-    #description;
-    #priority;
-    #project;
-    #dueDate;
-    #subtaskObj;
-    #completed;
-
-    
-set taskName(name) {
-    this.#taskName = name;
-}
-
-set description(description) {
+  constructor(
+    taskID,
+    taskName,
+    description,
+    priority,
+    project,
+    dueDate,
+    subtaskObj,
+    completed
+  ) {
+    this.#taskID = taskID;
+    this.#taskName = taskName;
     this.#description = description;
-}
-
-set priority(priority) {
     this.#priority = priority;
-}
-
-set project(project) {
     this.#project = project;
-}
-
-set dueDate(dueDate) {
     this.#dueDate = dueDate;
-}
+    this.#subtaskObj = subtaskObj;
+    this.#completed = completed;
+  }
 
-// Implement subtask feature here !!!
+  #taskID;
+  #taskName;
+  #description;
+  #priority;
+  #project;
+  #dueDate;
+  #subtaskObj;
+  #completed;
 
-set completed(completeStatus) {
-        if(typeof(completeStatus) === 'boolean') {
-            this.#completed = completeStatus;
-        };
+  set taskName(name) {
+    this.#taskName = name;
+  }
+
+  set description(description) {
+    this.#description = description;
+  }
+
+  set priority(priority) {
+    this.#priority = priority;
+  }
+
+  set project(project) {
+    this.#project = project;
+  }
+
+  set dueDate(dueDate) {
+    this.#dueDate = dueDate;
+  }
+
+  // Implement subtask feature here !!!
+
+  set completed(completeStatus) {
+    if (typeof completeStatus === "boolean") {
+      this.#completed = completeStatus;
     }
+  }
+
+  get taskName() {
+    return this.#taskName;
+  }
+
+  get description() {
+    return this.#description;
+  }
+
+  get priority() {
+    return this.#priority;
+  }
+
+  get project() {
+    return this.#project;
+  }
+
+  get dueDate() {
+    return this.#dueDate;
+  }
+
+  // Implement a getter that creates a copy of the subtask object here !!!
+
+  get completed() {
+    return this.#completed;
+  }
+
+  
 }
 
 // dummy dependencies
-
