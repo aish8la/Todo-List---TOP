8f910fc444d194195053d8e43f26d088fb056431
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-16 03:52:17 +0500

    change fields to public and remove accessors

--- src/taskManager.js
+++ src/taskManager.js
@@ -1,13 +1,7 @@
 export class TaskClass {
 
   #taskID;
-  #taskName;
-  #description;
-  #priority;
-  #project;
-  #dueDate;
-  #subTaskArr;
-  #completed;
+  completed = false;
 
   constructor(
     taskID,
@@ -17,85 +11,41 @@ export class TaskClass {
     project,
     dueDate,
     subTaskArr = [],
-    completed = false,
   ) {
     this.#taskID = taskID;
-    this.#taskName = taskName;
-    this.#description = description;
-    this.#priority = priority;
-    this.#project = project;
-    this.#dueDate = new Date(dueDate);
-    this.#subTaskArr = subTaskArr;
-    this.#completed = completed;
+    this.taskName = taskName;
+    this.description = description;
+    this.priority = priority;
+    this.project = project;
+
+    if (dueDate === "") {
+      this.dueDate = "";
+    } else {
+      this.dueDate = new Date(dueDate);
+    }
+
+    this.subTaskArr = subTaskArr;
+    this.completed = completed;
   }
 
   #createdDate = new Date()
 
-  set taskName(name) {
-    this.#taskName = name;
-  }
-
-  set description(description) {
-    this.#description = description;
-  }
-
-  set priority(priority) {
-    this.#priority = priority;
-  }
-
-  set project(project) {
-    this.#project = project;
-  }
-
-  set dueDate(dueDate) {
-    this.#dueDate = dueDate;
-  }
-
-  set completed(completeStatus) {
-    if (typeof completeStatus === "boolean") {
-      this.#completed = completeStatus;
-    }
-  }
-
-  get taskName() {
-    return this.#taskName;
-  }
-
-  get description() {
-    return this.#description;
-  }
-
-  get priority() {
-    return this.#priority;
-  }
-
-  get project() {
-    return this.#project;
-  }
-
-  get dueDate() {
-    return this.#dueDate;
-  }
-
-  get completed() {
-    return this.#completed;
+  get taskID() {
+    return this.#taskID;
   }
 
   get createdDate() {
-    return this.createdDate;
+    return this.#createdDate;
   }
 }
 
 export class SubTaskClass {
-  #subTaskDescription;
-  #subTaskCompleted;
-
   constructor(
     description,
     completed = false
   ) {
-    this.#subTaskDescription = description;
-    this.#subTaskCompleted = completed;
+    this.subTaskDescription = description;
+    this.subTaskCompleted = completed;
   }
 }
 
