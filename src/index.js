7e628cd4bc399b8ad7bad1529bd3a99fc5f5c630
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-16 03:51:42 +0500

    change lines for testing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import "./style.css";
 import { TaskClass } from "./taskManager";
 import { DataStoreClass } from "./dataStore";
 import { SubTaskClass } from "./taskManager";
+import { UIControllerClass } from "./UIController";
 
 // Instantiated Objects
 
 const App = new DataStoreClass(TaskClass, SubTaskClass);
+const UI = new UIControllerClass();
 
 // Sample Data 
 
 const taskData = {
+    taskID: "task-id-1",
     taskName: "Complete To-Do App",
     description: "Finalize all features for the to-do list app.",
     priority: "High",
@@ -31,3 +34,11 @@ const taskData = {
   };
 
 App.createTask(taskData);
+App.createTask(taskData);
+
+App.modfiyTask(
+    {
+        taskID: "task-id-1",
+        // taskName: "Changed",
+    }
+);
