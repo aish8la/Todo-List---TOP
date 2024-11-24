e89c41f7d4afe0508abc413a5d44b7ff15527e6b
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-11-24 23:24:24 +0500

    add priority sort and update view Array function

--- src/taskView.js
+++ src/taskView.js
@@ -1,15 +1,26 @@
 export class taskViewClass {
     constructor(taskObj) {
         this.taskDataObj = taskObj;
-        this.tasks = taskObj.taskArr || [];
-        
+        this.tasks = [...taskObj.taskArr] || [];
+    }
+
+    updateArr() {
+        this.tasks = [...this.taskDataObj.taskArr];
     }
 
     filterCompleteTasks() {
+        this.updateArr();
         const incompleteTasks = this.tasks.filter(e => {
             return !e.completed;
         });
         console.log(incompleteTasks);
     }
 
+    sortPriority() {
+        this.updateArr();
+        const priorityOder = {"High":1, "Medium": 2, "Low": 3};
+        const prioritySorted = this.tasks.toSorted((a, b) => {
+            return priorityOder[a.priority] - priorityOder[b.priority];
+        });
+    }
 }
