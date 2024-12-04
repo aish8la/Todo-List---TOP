4639d48c637ac5376a1df7afdf6d7d8b7353a77a
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-12-05 00:40:35 +0500

    change code formating

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,164 +1,180 @@
 import { formatDate } from "date-fns";
 
 export class DataStoreClass {
-    constructor(taskClass, subTaskClass) {
-        this.taskClass = taskClass;
-        this.subTaskClass = subTaskClass;
-    }
+  constructor(taskClass, subTaskClass) {
+    this.taskClass = taskClass;
+    this.subTaskClass = subTaskClass;
+  }
 
-    #taskArray = [
+  #taskArray = [
+    {
+      taskID: "task-id-3",
+      taskName: "Team Meeting",
+      description: "Discuss project progress and next steps.",
+      priority: "Low",
+      project: "Personal",
+      dueDate: "2024-11-30",
+      subTaskArr: [
         {
-            taskID: "task-id-3",
-            taskName: "Team Meeting",
-            description: "Discuss project progress and next steps.",
-            priority: "Low",
-            project: "Personal",
-            dueDate: "2024-11-30",
-            subTaskArr: [
-              { subTaskDescription: "Prepare meeting agenda", subTaskCompleted: true },
-              { subTaskDescription: "Share updates on tasks", subTaskCompleted: false },
-              { subTaskDescription: "Record meeting minutes", subTaskCompleted: false },
-            ],
-          },
-          {
-            taskID: "task-id-1",
-            taskName: "Complete To-Do App",
-            description: "Finalize all features for the to-do list app.",
-            priority: "High",
-            project: "Personal",
-            dueDate: "2024-12-01",
-            subTaskArr: [
-              { subTaskDescription: "Design UI", subTaskCompleted: true },
-              { subTaskDescription: "Implement Task CRUD functionality", subTaskCompleted: false },
-              { subTaskDescription: "Test app for bugs", subTaskCompleted: false },
-            ],
-          },
-          {
-            taskID: "task-id-2",
-            taskName: "Write Documentation",
-            description: "Create a detailed user guide for the app.",
-            priority: "Medium",
-            project: "Personal",
-            dueDate: "2024-12-05",
-            subTaskArr: [
-              { subTaskDescription: "Outline documentation structure", subTaskCompleted: true },
-              { subTaskDescription: "Write usage examples", subTaskCompleted: false },
-              { subTaskDescription: "Review for clarity", subTaskCompleted: false },
-            ],
-          },
-    ];
-    #projectListArr = ["Personal", "Work"];
+          subTaskDescription: "Prepare meeting agenda",
+          subTaskCompleted: true,
+        },
+        {
+          subTaskDescription: "Share updates on tasks",
+          subTaskCompleted: false,
+        },
+        {
+          subTaskDescription: "Record meeting minutes",
+          subTaskCompleted: false,
+        },
+      ],
+    },
+    {
+      taskID: "task-id-1",
+      taskName: "Complete To-Do App",
+      description: "Finalize all features for the to-do list app.",
+      priority: "High",
+      project: "Personal",
+      dueDate: "2024-12-01",
+      subTaskArr: [
+        { subTaskDescription: "Design UI", subTaskCompleted: true },
+        {
+          subTaskDescription: "Implement Task CRUD functionality",
+          subTaskCompleted: false,
+        },
+        { subTaskDescription: "Test app for bugs", subTaskCompleted: false },
+      ],
+    },
+    {
+      taskID: "task-id-2",
+      taskName: "Write Documentation",
+      description: "Create a detailed user guide for the app.",
+      priority: "Medium",
+      project: "Personal",
+      dueDate: "2024-12-05",
+      subTaskArr: [
+        {
+          subTaskDescription: "Outline documentation structure",
+          subTaskCompleted: true,
+        },
+        { subTaskDescription: "Write usage examples", subTaskCompleted: false },
+        { subTaskDescription: "Review for clarity", subTaskCompleted: false },
+      ],
+    },
+  ];
+  #projectListArr = ["Personal", "Work"];
 
-    #lastID = 0;
+  #lastID = 0;
 
-    #idGen() {
-        return `task-id-${this.#lastID++}`;
-    }
+  #idGen() {
+    return `task-id-${this.#lastID++}`;
+  }
 
-    #createSubTask(subTaskDataArr) {
-        const newArr = [];
-        subTaskDataArr.forEach((element) => {
-            const newSubTask = new this.subTaskClass(
-                element.subTaskDescription,
-                element.subTaskCompleted
-            )
-            newArr.push(newSubTask);
-        });
-        return newArr;
-    }
+  #createSubTask(subTaskDataArr) {
+    const newArr = [];
+    subTaskDataArr.forEach((element) => {
+      const newSubTask = new this.subTaskClass(
+        element.subTaskDescription,
+        element.subTaskCompleted
+      );
+      newArr.push(newSubTask);
+    });
+    return newArr;
+  }
 
-    createTask(dataObject) {
-        const data = dataObject;
-        const task = new this.taskClass(
-            this.#idGen(),
-            data.taskName,
-            data.description,
-            data.priority,
-            data.project,
-            data.dueDate,
-            this.#createSubTask(data.subTaskArr),
-        );
-        this.#taskArray.push(task);
-        console.log(this.#taskArray);
-    };
+  createTask(dataObject) {
+    const data = dataObject;
+    const task = new this.taskClass(
+      this.#idGen(),
+      data.taskName,
+      data.description,
+      data.priority,
+      data.project,
+      data.dueDate,
+      this.#createSubTask(data.subTaskArr)
+    );
+    this.#taskArray.push(task);
+    console.log(this.#taskArray);
+  }
 
-    modfiyTask(dataObj) {
-        const data = dataObj;
-        const taskElementID = this.#taskArray.findIndex(element => {
-            return element.taskID === data.taskID;
-        });
+  modfiyTask(dataObj) {
+    const data = dataObj;
+    const taskElementID = this.#taskArray.findIndex((element) => {
+      return element.taskID === data.taskID;
+    });
 
-        const taskObj = this.#taskArray[taskElementID];
-        
-        for (let key in taskObj) {
-            //skips the code if the current key is "completed"
-            if(key === "completed") {
-                continue;
-            }
-            // Leaves as with an empty string if the key is not defined in the new data
-            if(data[key] === undefined) {
-                taskObj[key] = "";
-            } else {
-                // if current key is dueDate then handles empty strings
-                if(key === "dueDate") {
-                    taskObj[key] = data.dueDate === "" ? "" : new Date(data.dueDate);
-                }
-                taskObj[key] = data[key];
-            }
+    const taskObj = this.#taskArray[taskElementID];
+
+    for (let key in taskObj) {
+      //skips the code if the current key is "completed"
+      if (key === "completed") {
+        continue;
+      }
+      // Leaves as with an empty string if the key is not defined in the new data
+      if (data[key] === undefined) {
+        taskObj[key] = "";
+      } else {
+        // if current key is dueDate then handles empty strings
+        if (key === "dueDate") {
+          taskObj[key] = data.dueDate === "" ? "" : new Date(data.dueDate);
         }
+        taskObj[key] = data[key];
+      }
+    }
+  }
+
+  taskCompleteToggle(taskID) {
+    const taskIndex = this.#taskArray.findIndex((e) => {
+      return taskID === e.taskID;
+    });
+    this.#taskArray[taskIndex].toggleComplete();
+  }
+
+  deleteTask(taskID) {
+    const taskIndex = this.#taskArray.findIndex((e) => {
+      return taskID === e.taskID;
+    });
+
+    if (taskIndex === -1) {
+      return console.log("The task you are trying to delete does not exist"); //TODO: Add error handling
     }
 
-    taskCompleteToggle(taskID) {
-        const taskIndex = this.#taskArray.findIndex(e => {
-            return taskID === e.taskID;
-        });
-        this.#taskArray[taskIndex].toggleComplete();
+    this.#taskArray.splice(taskIndex, 1);
+  }
+
+  addProject(prjName) {
+    if (
+      this.#projectListArr.some((str) => {
+        return str.toLowerCase() === prjName.toLowerCase();
+      })
+    ) {
+      return console.log("Project with that name already exists"); //TODO: Add propper error handling
     }
 
-    deleteTask(taskID) {
-        const taskIndex = this.#taskArray.findIndex(e => {
-            return taskID === e.taskID;
-        });
+    this.#projectListArr.push(prjName);
+  }
 
-        if(taskIndex === -1) {
-            return console.log("The task you are trying to delete does not exist"); //TODO: Add error handling
-        }
-        
-        this.#taskArray.splice(taskIndex, 1);
+  deleteProject(prjName) {
+    if (
+      this.#taskArray.some((task) => {
+        return task.project.toLowerCase() === prjName.toLowerCase();
+      })
+    ) {
+      return console.log("The project you are trying to delete contains tasks"); //TODO: Add a error handler for this
     }
 
-    addProject(prjName) {
+    const indexProject = this.#projectListArr.findIndex((project) => {
+      return project.toLowerCase() === prjName.toLowerCase();
+    });
 
-        if(this.#projectListArr.some(str => {
-            return str.toLowerCase() === prjName.toLowerCase()
-        })) {
-            return console.log("Project with that name already exists"); //TODO: Add propper error handling
-        }
-
-        this.#projectListArr.push(prjName);
+    if (indexProject === -1) {
+      return console.log("The project you are trying to delete does not exist"); //TODO: Add error handlings
     }
 
-    deleteProject(prjName) {
+    this.#projectListArr.splice(indexProject, 1);
+  }
 
-        if(this.#taskArray.some(task => {
-            return task.project.toLowerCase() === prjName.toLowerCase();
-        })) {
-            return console.log("The project you are trying to delete contains tasks"); //TODO: Add a error handler for this
-        }
-
-        const indexProject = this.#projectListArr.findIndex(project => {
-            return project.toLowerCase() === prjName.toLowerCase();
-        });
-
-        if(indexProject === -1){
-            return console.log("The project you are trying to delete does not exist"); //TODO: Add error handlings
-        }
-
-        this.#projectListArr.splice(indexProject,1);
-    }
-
-
-    get taskArr() {
-        return this.#taskArray;
-    }
+  get taskArr() {
+    return this.#taskArray;
+  }
 }
--- src/taskView.js
+++ src/taskView.js
@@ -1,43 +1,50 @@
 import { compareAsc, differenceInCalendarDays } from "date-fns";
 
 export class taskViewClass {
-    constructor(taskObj) {
-        this.taskDataObj = taskObj;
-        this.tasks = [...taskObj.taskArr] || [];
-    }
+  constructor(taskObj) {
+    this.taskDataObj = taskObj;
+    this.tasks = [...taskObj.taskArr] || [];
+  }
 
-    updateArr() {
-        this.tasks = [...this.taskDataObj.taskArr];
-    }
+  updateArr() {
+    this.tasks = [...this.taskDataObj.taskArr];
+  }
 
-    filterCompleteTasks() {
-        this.updateArr();
-        const incompleteTasks = this.tasks.filter(e => {
-            return !e.completed;
-        });
-        return incompleteTasks;
-    }
+  filterCompleteTasks() {
+    this.updateArr();
+    const incompleteTasks = this.tasks.filter((e) => {
+      return !e.completed;
+    });
+    return incompleteTasks;
+  }
 
-    filterDayDifference(numOfDays) {
-        const currentDate = new Date();
-        const dayDiffFilteredTask = this.tasks.filter(e => {
-            return differenceInCalendarDays(e.dueDate, currentDate) <= numOfDays;
-        });
-        return dayDiffFilteredTask;
-    }
+  filterDayDifference(numOfDays) {
+    const currentDate = new Date();
+    const dayDiffFilteredTask = this.tasks.filter((e) => {
+      return differenceInCalendarDays(e.dueDate, currentDate) <= numOfDays;
+    });
+    return dayDiffFilteredTask;
+  }
 
-    sortPriority() {
-        this.updateArr();
-        const priorityOder = {"High":1, "Medium": 2, "Low": 3};
-        const prioritySorted = this.tasks.toSorted((a, b) => {
-            return priorityOder[a.priority] - priorityOder[b.priority];
-        });
-        return prioritySorted;
-    }
+  filterProject(project) {
+    const filterByProject = this.tasks.filter((task) => {
+      return task.project.toLowerCase() === project.toLowerCase();
+    });
+    return filterByProject;
+  }
 
-    sordDue() {
-        this.updateArr();
-        const sortedAscDue = this.tasks.toSorted(compareAsc);
-        return sortedAscDue;
-    }
+  sortPriority() {
+    this.updateArr();
+    const priorityOder = { High: 1, Medium: 2, Low: 3 };
+    const prioritySorted = this.tasks.toSorted((a, b) => {
+      return priorityOder[a.priority] - priorityOder[b.priority];
+    });
+    return prioritySorted;
+  }
+
+  sordDue() {
+    this.updateArr();
+    const sortedAscDue = this.tasks.toSorted(compareAsc);
+    return sortedAscDue;
+  }
 }
