135ec97021955b244eb05796ceb7305285c9ddfb
aish8la <64914670+aish8la@users.noreply.github.com.>
2024-12-16 01:48:10 +0500

    remove test comments

--- src/index.js
+++ src/index.js
@@ -16,25 +16,3 @@ const UIObject = new UIControllerClass(dataObj, displayObj, taskView);
   
 // Initialize App
 UIObject.initializeApp(defaultData);
-
-//Test Functions
-// UIObject.addTaskClick();
-// const form = document.querySelector('input[name="taskName"]');
-// form.value = "TAsk";
-// UIObject.addNewSubtask();
-// UIObject.addNewSubtask();
-// UIObject.saveNewTask(document.querySelector("form"));
-
-
-
-
-
-
-// console.log(app.taskArr);
-// displayObj.displayInit();
-
-// displayObj.renderForm("task-id-4");
-// displayObj.newSubTaskRender();
-// console.log(taskView.tasks);
-// console.log(displayObj.taskViewObj.findTaskByID("task-id-4"));
-// console.log(taskView.findTaskByID("task-id-4"));
